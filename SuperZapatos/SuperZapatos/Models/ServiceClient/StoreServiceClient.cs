50f86412972972f2f9c17554acd95efb398ef3e8
jnvarco <jnvarco@gmail.com>
2017-01-23 22:00:48 -0600

    Prueba Johan Vargas

---
+++ SuperZapatos/SuperZapatos/Models/ServiceClient/StoreServiceClient.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace SuperZapatos.Models.ServiceClient
+{
+    public class StoreServiceClient
+    {
+        private String BASE_URL = "http://localhost:64470/services/Stores/";
+
+        
+        public Object[] GetStores()
+        {
+            WebClient storeClient = new WebClient();
+            var content = storeClient.DownloadString(this.BASE_URL);
+            Object[] data = JsonConvert.DeserializeObject<Object[]>(content);
+
+            return data;
+        }
+    }
+}
